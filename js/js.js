eafc20a03784bca01240ea309197ef88a637d9b2
zh-kaiii <bthd0404@gmail.com>
2020-11-13 16:26:18 +0800

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".circle").click(function(){
+        $(".outer").hide(800);
+    });
+    $(".circle").click(function(){
+        $(".inside").show(800);
+    });
+})
